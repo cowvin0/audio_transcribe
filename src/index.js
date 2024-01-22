5fd1c59c140ea979338f206e662a50b80ad1c2fa
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-01-22 16:25:14 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+         from: process.env.FROM,
+         body: 'Hello there!',
+         to: 'whatsapp:+558386136318'
+       })
+      .then(message => console.log(message.sid));
