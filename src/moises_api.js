ff081640307b52d743afef9071b8e8d612418525
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-01-24 17:15:11 -0300

    adding text to speech

--- src/moises_api.js
+++ src/moises_api.js
@@ -25,3 +25,36 @@ export async function transcribeMoises() {
         console.log("Error:", error);
     }
 }
+
+export async function TextToSpeech(textMessage) {
+    try {
+
+        fs.writeFile("cf.txt", textMessage, (err) => {
+            if (err) throw err;
+            console.log("File has been saved.");
+        });
+
+        const downloadUrl = await moises.uploadFile('./src/text/cf.txt');
+        console.log('Arquivo enviado para o Moises:', downloadUrl);
+
+        const jobId = await moises.addJob(
+            "job-1t",
+            "TextToAudio",
+            {InputText: downloadUrl}
+        );
+        console.log('Job adicionado:', jobId);
+
+        const job = await moises.waitForJobCompletion(jobId);
+        console.log('Job concluído:', job);
+
+        if (job.status === 'SUCCEEDED') {
+            const files = await moises.downloadJobResults(job, './src/results');
+            console.log('Arquivos baixados:', files);
+        } else{
+            console.log('Job falhou:', job);
+        }
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
+
